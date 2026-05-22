b363a57a86c2ef1640840fe04bfec065b5170783
koduhai <koduhai@koduhai.com>
2026-05-21 19:48:44 -0700

    feat: add VisuallyHidden primitive and primitives barrel

---
+++ src/primitives/index.ts
@@ -0,0 +1,7 @@
+export { mergeRefs } from './mergeRefs';
+export { composeEventHandlers } from './composeEventHandlers';
+export { useId } from './useId';
+export { useControllableState } from './useControllableState';
+export { Slot } from './Slot';
+export type { SlotProps } from './Slot';
+export { VisuallyHidden } from './VisuallyHidden';
